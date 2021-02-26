17be813f1f7291b1ebb527f8da7614a3006fbf8e
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-25 22:01:45 -0600

    installing and setting up boiler plate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+  const [feelingInput, setFeelingInput] = useState('');
+
+  const saveFeelingInput = function (e) {
+    console.log('target value: ', e.target.value);
+    setFeelingInput(e.target.value);
+    console.log('feeling input is: ', feelingInput);
+  }; // end saveFeelingInput
 
   return (
     <div className='App'>
@@ -10,6 +19,12 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+        <p>How are you feeling today? 1 is Terrible and 5 is Fantastic</p>
+        <input type="number" 
+        value={feelingInput} 
+        placeholder="Enter a number" 
+        onChange={saveFeelingInput}
+        />
     </div>
   );
 }
