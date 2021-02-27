ec6b23a988c1a51557fb74a5ef960e63f0b7ae32
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-26 19:03:30 -0600

    imported the selector and setting up to send it

--- src/components/Submit/submit.jsx
+++ src/components/Submit/submit.jsx
@@ -1,14 +1,30 @@
 import {useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 const Submit = function () {
   const history = useHistory();
+
+  // this gives us all the values entered.
+  // i did one per page
+  const feeling = useSelector(store => store.feelingReducer);
+  const understand = useSelector(store => store.understandReducer);
+  const supported = useSelector(store => store.supportedReducer);
+  const comments = useSelector(store => store.commentsReducer);
+  
+
+  const submitInfo = function () {
+
+    
+    // go back to home page
+    history.push('/');
+  }
 
   return (
     <div>
       <p>
         this is the submit page
       </p>
-      <button onClick={() => history.push('/')}>Start Over</button>
+      <button onClick={submitInfo}>Finalize Info</button>
     </div>
   )
 };
